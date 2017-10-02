e244825603ac943f5b70541642d1736909df9b7d
Shiva <skandaswami@ufl.edu>
2017-10-01 20:44:07 -0400

    update cors

--- qchain_backend/settings.py
+++ qchain_backend/settings.py
@@ -124,6 +124,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
